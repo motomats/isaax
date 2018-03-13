495f10bf11cc60c629e1767ffc44adf6335fe8c6
MATSUI, Motokatsu <j3tm0t0@gmail.com>
2018-03-13 23:40:00 +0900

    change sleep

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ sense.set_rotation(180)
 while True:
     temp = sense.get_temperature()
     print("Temperature: %s C" % temp)
-    sense.show_message("Temperature: %s C" % temp)
-    time.sleep(10)
+    sense.show_message("Temp: %s C" % temp)
+    time.sleep(60)
