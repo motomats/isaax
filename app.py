92ea330fc6cb3655125cc491ed03a0c5ea477e96
MATSUI, Motokatsu <j3tm0t0@gmail.com>
2018-03-13 19:53:06 +0900

    use sensehat

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import time
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.set_rotation(180)
 
 while True:
     print("Hello IoT from Isaax")
-    time.sleep(5)
+    sense.show_message("Hello IoT from Isaax")
+    time.sleep(60)
