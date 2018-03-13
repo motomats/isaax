a1a65e06ff48d5eeee1e3e1c36961342f1ce4c6f
MATSUI, Motokatsu <j3tm0t0@gmail.com>
2018-03-13 23:38:44 +0900

    infinite loop

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(180)
 
-for i in range(5):
-    print("Hello IoT from Isaax" + str(i) )
-    sense.show_message("Hello IoT from Isaax")
+while True:
     temp = sense.get_temperature()
     print("Temperature: %s C" % temp)
+    sense.show_message("Temperature: %s C" % temp)
     time.sleep(10)
